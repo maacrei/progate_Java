4346fba24f4e7fd0cfc176b963e9eb668d0dc909
神原 茉里 <maacrei5723@gmail.com>
2022-05-17 18:05:01 +0900

    lesson1

---
+++ Java1_01/Main.java
@@ -0,0 +1,9 @@
+// Javaの基本 //
+
+// Javaに触れてみよう
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello Java");
+  }
+}
+// 「System.out.println」は出力(表示)せよという命令
---
+++ Java1_02/Main.java
@@ -0,0 +1,9 @@
+// Javaの基本 //
+
+// Javaの基本・文字列
+class Main {
+  public static void main(String[] args) {
+    // 「こんにちは、Java」と出力してください
+    System.out.println("こんにちは、Java");
+  }
+}
