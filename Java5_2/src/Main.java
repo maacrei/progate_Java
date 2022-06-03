fc9bd5605f6faf72fcfcfce1e810d58dce0c9960
神原 茉里 <maacrei5723@gmail.com>
2022-06-03 15:02:20 +0900

    finish

---
+++ Java5_2/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+	public static void main(String[] args) {
+		Car car = new Car();
+		Bicycle bicycle = new Bicycle();
+	}
+}
---
+++ Java5_7/src/Bicycle.java
@@ -0,0 +1,7 @@
+public class Bicycle extends Vehicle {
+	// Bicycleクラスのコンストラクタを定義し、
+	  // superを用いてスーパークラスのコンストラクタを呼び出してください
+	  Bicycle(String name, String color) {
+	    super(name, color);
+	  }
+}
